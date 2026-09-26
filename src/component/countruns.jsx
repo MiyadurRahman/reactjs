09e22f235a66da3573277c8acea6a74236d9e2ab
Meyadur Rahman <100347980+MiyadurRahman@users.noreply.github.com>
2026-09-26 23:39:05 +0600

    usestate

--- src/component/countruns.jsx
+++ src/component/countruns.jsx
@@ -7,16 +7,19 @@ export default function Hits(){
        setCount(updatesingle)
         
     }
-    const  four =()=> {
+   const  four =()=> {
        const updatesingle=count+4;
        setCount(updatesingle)
-        
-    }
+          
+   }
+   const  six =()=> {
+       setCount(previous=>previous+6)
+   }
 return(<>
     <h1>what the batsmn hits</h1>
     <button onClick={singles}>single:{count}</button>
     <button onClick={four}>four: {count}</button>
-    <button>six:</button>
+    <button onClick={six}>six: {count}</button>
     
     
     
